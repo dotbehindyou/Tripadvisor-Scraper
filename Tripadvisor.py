87dc8818e59a4f6d8f52d41ccff4559eb2b59be3
Christopher Mogler <christophermogler@outlook.de>
2020-07-19 21:26:37 +0200

    Url wird dynamisch erzeugt

--- Tripadvisor.py
+++ Tripadvisor.py
@@ -2,13 +2,14 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = 'https://www.tripadvisor.com/Hotel_Review-g1053569-d1112186-Reviews-Zur_Alten_Brucke-Schiltach_Baden_Wurttemberg.html'
-
-
 def saveJson(filename, dataJson):
     with open('data/'+filename+'.json','w') as outfile:
         json.dump(dataJson, outfile)
 
-def getContent():
+def getContent(url):
     page = requests.get(url)
     return BeautifulSoup(page.content, 'html.parser')
+
+def generateUrl(g, d):
+    return "https://www.tripadvisor.com/g"+str(g)+"-d"+str(d)
+    
